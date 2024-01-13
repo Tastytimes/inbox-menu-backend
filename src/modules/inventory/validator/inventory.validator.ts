429fd2dcbc2a5cfd8ea5cf152fe935a3f87f1d3c
madhav <madhav@fuzeindia.com>
2024-01-13 21:31:37 +0530

    inventry

--- src/modules/inventory/validator/inventory.validator.ts
+++ src/modules/inventory/validator/inventory.validator.ts
@@ -1,20 +1,15 @@
 import Joi from "joi";
 import { IInventory } from "../model/inventory.model";
-;
-
-
 const inventoryValidation = (regData: IInventory) => {
-    const dataSchema = Joi.object({
-        foodType: Joi.string().required(),
-        category: Joi.string().required(),
-        subCategory: Joi.string().required(),
-        itemName: Joi.string().required(),
-        sellerPrice: Joi.number().required(),
-        isActive: Joi.string().required()
-    });
-    return dataSchema.validate(regData, { abortEarly: false });
-}
+  const dataSchema = Joi.object({
+    foodType: Joi.string().required(),
+    category: Joi.string().required(),
+    subCategory: Joi.string().required(),
+    itemName: Joi.string().required(),
+    sellerPrice: Joi.number().required(),
+    isActive: Joi.boolean().required(),
+  });
+  return dataSchema.validate(regData, { abortEarly: false });
+};
 
-
-
-export { inventoryValidation }
+export { inventoryValidation };
